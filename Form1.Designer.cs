e2524a326290c4e422b5de6c34872ef0ddc2882e
Kevin <Kevin@MSI>
2023-06-05 18:57:14 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Series
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Reset = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SetStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(200, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get next in Fibonacci";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(540, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(200, 246);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(168, 39);
+            this.Reset.TabIndex = 2;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(409, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Current:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(414, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // SetStart
+            // 
+            this.SetStart.Location = new System.Drawing.Point(200, 132);
+            this.SetStart.Name = "SetStart";
+            this.SetStart.Size = new System.Drawing.Size(168, 40);
+            this.SetStart.TabIndex = 5;
+            this.SetStart.Text = "Set Start of Series";
+            this.SetStart.UseVisualStyleBackColor = true;
+            this.SetStart.Click += new System.EventHandler(this.SetStartSeries);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SetStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Fibonacci";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SetStart;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Series
+{
+    public partial class Form1 : Form
+    {
+
+        private ISeries series = new Fibonacci();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+           
+        {
+            this.label1.Text = series.GetNext().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.series.Reset();
+            this.label1.Text = "0";
+        }
+
+        private void SetStartSeries(object sender, EventArgs e)
+        {
+            try
+            {
+                String inputTextStart = this.textBox1.Text;
+                    
+                if(inputTextStart == null)
+                {
+                    return;
+                }
+
+                int parsedInput = Int32.Parse(inputTextStart);
+                this.series.SetStart(parsedInput);
+                this.label1.Text = series.GetNext().ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Should provide an integer");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public interface ISeries
+    {
+        int GetNext(); // devolver el sig. Número en la serie  
+        void Reset(); // reiniciar  
+        void SetStart(int x); // establece valor de inicio 
+    }
+
+    public class Fibonacci : ISeries
+    {
+        public int currentVal;
+        public int indexCurrentVal;
+
+        public Fibonacci()
+        {
+            this.currentVal = 0;
+            this.indexCurrentVal = 0;
+        }
+
+        private int explicitFormulaFibo(int index)
+        {
+            return (int)Math.Round((Math.Pow((1 + Math.Sqrt(5)), index) -
+         Math.Pow((1 - Math.Sqrt(5)), index)) / (Math.Pow(2, index) * Math.Sqrt(5)));
+        }
+
+        public int GetNext()
+        {
+            this.indexCurrentVal++;
+            this.currentVal = this.explicitFormulaFibo(this.indexCurrentVal);
+            return this.currentVal;
+        }
+
+        public void Reset()
+        {
+            this.currentVal = 0;
+            this.indexCurrentVal = 0;
+        }
+
+        public void SetStart(int x)
+        {
+            this.indexCurrentVal = x - 1;
+        }
+    }
+
+    public class Squares : ISeries
+    {
+        public int currentVal;
+        public int indexCurrentVal;
+
+        public Squares()
+        {
+            this.currentVal = 0;
+            this.indexCurrentVal = 0;
+        }
+
+        public int GetNext()
+        {
+            this.indexCurrentVal++;
+            this.currentVal = (int)Math.Pow(this.indexCurrentVal, 2);
+            return this.currentVal;
+        }
+
+        public void Reset()
+        {
+            this.currentVal = 0;
+            this.indexCurrentVal = 0;
+        }
+
+        public void SetStart(int x)
+        {
+            this.indexCurrentVal = x - 1;
+        }
+    }
+}
